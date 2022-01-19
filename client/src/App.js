fa5859ae6d31bf9b7529fe5a1b85169666642d41
Austin Furlong <furlong.austin@gmail.com>
2022-01-19 14:32:35 -0500

    working app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-
+import { BrowserRouter as Router, Redirect, Route, Switch, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 import './App.css';
+import AuthContext from "./AuthContext";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/register">
+            <h1>Register</h1>
+          </Route>
+          <Route path="/about/us">
+            <h1>About Us</h1>
+          </Route>
+          <Route path="/profile/:profileId">
+            <h1>Profile</h1>
+          </Route>
+          <Route path="/adventure/planning">
+            <h1>Adventure Planning</h1>
+          </Route>
+          <Route path="/travel/buddy/add">
+            <h1>Add Travel Buddy</h1>
+          </Route>
+          <Route path="/trip/overview">
+            <h1>Trip Overview</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route>
+            <h1>Not Found</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
+    </>
   );
 }
 
