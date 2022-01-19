b05f7ff9142c55cb566d7e263cd6a6bd89e01fc1
Austin Furlong <furlong.austin@gmail.com>
2022-01-19 10:25:39 -0500

    added test and resources

---
+++ server/src/test/java/adventure/time/data/TripJdbcTemplateRepositoryTest.java
@@ -0,0 +1,12 @@
+package adventure.time.data;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripJdbcTemplateRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
